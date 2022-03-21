3b0dc7cabb108e96324d4369415c9a06977c14c0
Adam Gausmann <agausmann@fastmail.com>
2022-03-21 13:58:39 -0500

    Also print min,max for aggregate delay analysis

--- simulator/src/bin/delay_averages.rs
+++ simulator/src/bin/delay_averages.rs
@@ -4,7 +4,8 @@ const BITS: [u32; 5] = [4, 6, 8, 10, 12];
 const GROUP_SIZES: [(u32, fn(i32, i32, u32) -> Results); 2] = [(3, booth3), (4, booth4)];
 
 fn main() {
-    println!("Bits,Average delay (Booth3),Average delay (Booth4)");
+    println!("Bits,Booth3,,,Booth4,,");
+    println!(",Avg,Min,Max,Avg,Min,Max");
     for bits in BITS {
         print!("{}", bits);
         for (_, booth_fn) in GROUP_SIZES {
@@ -14,8 +15,12 @@ fn main() {
                     delays.push(booth_fn(a, b, bits).delay);
                 }
             }
-            let n = delays.len();
-            print!(",{}", (delays.into_iter().sum::<u32>() as f32) / (n as f32));
+            print!(
+                ",{},{},{}",
+                (delays.iter().sum::<u32>() as f32) / (delays.len() as f32),
+                delays.iter().min().unwrap(),
+                delays.iter().max().unwrap(),
+            );
         }
         println!();
     }
